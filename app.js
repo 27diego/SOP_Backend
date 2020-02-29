af90fb0c79e9f6145017c51cd9a1e8c98054cf46
27diego <divegamaravilla@csumb.edu>
2020-02-28 20:26:34 -0800

    preparing docker files

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const mongoose = require("mongoose");
 
 const server = express();
+
+server.use(bodyParser.json());
+// server.use(multer({storage: "/SOPs", }))
 
 server.get("/", (req, res) => {
   res.json("Its Working");
---
+++ controllers/Files.js
@@ -0,0 +1,1 @@
+const fileUpload = (req, file, cb) => {};
