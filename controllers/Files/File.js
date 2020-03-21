273750c8205bad0600c70f63fc08e62f185c6d93
27diego <divegamaravilla@csumb.edu>
2020-03-20 21:16:27 -0700

    added renaming file feature and turning it into a pdf feature

--- controllers/Files/File.js
+++ controllers/Files/File.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const word2pdf = require("word2pdf");
+
 const mongoSOPS = require("../../model/SopLibrary");
 const multer = require("multer");
 
@@ -17,9 +20,6 @@ const addFile = (req, res) => {
 
   const location = file.path;
 
-  console.log("file: ", file);
-  console.log(title);
-
   sopDetails = { title, category, department, location };
   const sop = new mongoSOPS(sopDetails);
 
@@ -29,9 +29,47 @@ const addFile = (req, res) => {
       res.json("File Saved!");
     })
     .catch(err => console.log(err));
+
+  //rename file
+
+  rename(file.path, title);
 };
 
 const getFile = (req, res) => {};
+
+const rename = (path, title) => {
+  const indexOfSlash = path.lastIndexOf("/");
+  const indexOfDot = path.lastIndexOf(".");
+
+  const prefix = path.substring(0, indexOfSlash + 1);
+  const postFix = path.substring(indexOfDot);
+
+  const newName = prefix + title + postFix;
+
+  fs.renameSync(path, newName);
+
+  transformFile(newName);
+};
+
+const transformFile = async path => {
+  const data = await word2pdf(path);
+
+  const indexOfDot = path.lastIndexOf(".");
+  const finalName = path.substring(0, indexOfDot + 1) + "pdf";
+
+  let error = false;
+
+  fs.writeFileSync(finalName, data, err => {
+    if (err) {
+      console.log(err);
+      error = true;
+    }
+  });
+
+  if (error == false) {
+    fs.unlinkSync(path);
+  }
+};
 
 module.exports = {
   addFile,
